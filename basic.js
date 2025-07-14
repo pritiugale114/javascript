e277ccf453cdf7189f082b805e80d88fc4f07f49
pritiugale114 <pritiugale114@gmail.com>
2025-07-14 22:25:06 +0530

    all javascript basics

---
+++ basic.js
@@ -0,0 +1,30 @@
+
+console.log("Hello world");
+let num = 2;
+const name='priti';
+var age=21;
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof num);
+let stud = "priti ugale";
+console.log(stud.length);
+console.log(stud[0]);
+console.log(stud[1]);
+console.log(stud[2]);
+console.log(stud[3]);
+console.log(`hello my name is ${stud}`);
+
+let str="my naame is priti ugale";
+console.log(str.trim());
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.indexOf("p"));
+//method chaining
+// console.log(str.toUpperCase.trim()); //not working
+
+console.log(str.slice(3));
+console.log(str.slice(1,4));
+console.log(str.slice(-3));
+
+console.log(str.replace("priti" , "vaibhav")); //replace
+console.log(str.repeat(2)); //repeat
